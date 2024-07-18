b6612bf5f64a291ea23f8ae5848070b2ac4bc6a0
luiscacostas <acosta.luisc15@gmail.com>
2024-07-18 11:19:05 +0200

    fix lint

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+
 
 const Button = ({ increment, onClickFunction }) => {
-  const handleClick = () => {
+  const handleClick = (increment) => {
     onClickFunction(increment)
   }
   return <button onClick={handleClick}>+{increment}</button>
